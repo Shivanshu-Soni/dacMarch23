6203ba3e768555fec47860264866ac180df1defe
Shivanshu-Soni <127841605+Shivanshu-Soni@users.noreply.github.com>
2023-03-17 15:22:20 +0530

    Add files via upload

---
+++ Star2.java
@@ -0,0 +1,25 @@
+class Star2
+{
+	public static void main(String[] args)
+	{
+		for(int i=5;i>=1;i--)
+		{
+			for(int k=i-1;k<=i;k++)
+			{
+				System.out.print(" ");
+			}	
+	
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	
+	}
+
+
+
+
+}
